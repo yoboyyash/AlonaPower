242d26557bfa1d89c5a27a4ff21fc7b08d6650c4
yoboyyash <yashrajbaviskar@gmail.com>
2025-02-27 17:51:40 -0600

    Pre deployment commit

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/AlonaPower/",
+  base: "/AlonaPower",
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+});
